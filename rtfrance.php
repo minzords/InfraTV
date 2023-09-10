40e79a7ec94a1517409c438112e1f194b26bb4a5
Florian Blanchet <contact@minzord.fr>
2023-09-10 21:38:08 +0200

    Add icon program

--- rtfrance.php
+++ rtfrance.php
@@ -33,17 +33,34 @@ for ($i = 2; $i < count($data); $i++) {
     $programme->addCountry(new Tv\Elements\Country('Russie', 'ru'));
     $programme->language =  new Tv\Elements\Language('fr');
 
-    if ($data[$i][3] == "JOURNAL D'ACTUALITE") {
-        $programme->premiere = new Tv\Elements\Premiere('Inedit');
-        $programme->lastChance = new Tv\Elements\LastChance('Dernière diffusion');
+    switch($data[$i][3]){
+        case "JOURNAL D'ACTUALITE":
+            $programme->premiere = new Tv\Elements\Premiere('Inedit');
+            $programme->lastChance = new Tv\Elements\LastChance('Dernière diffusion');
+            break;
+        case "DOCUMENTAIRE":
+            $programme->addIcon(new Tv\Elements\Icon('https://mf.b37mrtl.ru/french/images/2018.01/thumbnail/5a6f431709fac2e5558b4567.png'));
+            break;
+        case "L'ECHIQUIER MONDIAL":
+            $programme->addIcon(new Tv\Elements\Icon('https://mf.b37mrtl.ru/french/images/2023.05/original/646e2dd06f7ccc66d1525226.png'));
+            break;
+        case "POLIT'MAG":
+            $programme->addIcon(new Tv\Elements\Icon('https://mf.b37mrtl.ru/french/images/2022.06/original/629b56716f7ccc25e00b8f80.png'));
+            break;
+        case "LA GRANDE INTERVIEW":
+            $programme->addIcon(new Tv\Elements\Icon('https://mf.b37mrtl.ru/french/images/2019.04/original/5ca3568809fac2a7758b4567.PNG'));
+            break;
+        case "AFRICONNECT":
+            $programme->addIcon(new Tv\Elements\Icon('https://mf.b37mrtl.ru/french/images/2021.09/original/614a6b5a87f3ec39ff641e09.PNG'));
+            break;
+        default:
+            $programme->addIcon(new Tv\Elements\Icon('https://i.f1g.fr/media/eidos/orig/2019/10/29/XVM5b3bd926-f975-11e9-ba50-e460b16f9313.jpg'));
     }
 
-    $programme->addIcon(new Tv\Elements\Icon('https://i.f1g.fr/media/eidos/orig/2019/10/29/XVM5b3bd926-f975-11e9-ba50-e460b16f9313.jpg'));
     $tv->addProgramme($programme);
 }
 
 $tv->addChannel($channel);
-$tv->addProgramme($programme);
 
 $xmltv = new XmlTv();
 $xml = $xmltv->generate($tv);
